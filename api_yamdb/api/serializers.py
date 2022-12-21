49d497e958d0ed3da956f3a83ef2511f737bdd3b
Guzovvv <guzovvv@yandex.ru>
2022-12-21 17:03:50 +0300

    SP16v4

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,15 +8,12 @@ from reviews.models import Category, Comment, Genre, Review, Title, User
 
 class ConfirmationSerializer(serializers.ModelSerializer):
     def validate(self, data):
-        if data["username"] == "me":
+        if data['username'] == 'me':
             raise ValidationError("Нельзя использовать имя 'me'")
         return data
 
     class Meta:
-        fields = (
-            "username",
-            "email",
-        )
+        fields = ('username', 'email',)
         model = User
 
 
@@ -31,72 +28,81 @@ class UsersSerializer(serializers.ModelSerializer):
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
-            raise ValidationError("Пользователь с такой почтой " "уже зарегистрирован")
+            raise ValidationError('Пользователь с такой почтой '
+                                  'уже зарегистрирован')
         return value
 
     def validate_username(self, value):
-        if User.objects.filter(username=value).exists():
-            raise ValidationError("Пользователь с таким именем " "уже зарегистрирован")
+        if value == 'me':
+            raise ValidationError('Недопустимое имя пользователя!')
+        elif User.objects.filter(username=value).exists():
+            raise ValidationError('Пользователь с таким именем '
+                                  'уже зарегистрирован')
         return value
 
     class Meta:
         model = User
         fields = (
-            "first_name",
-            "last_name",
-            "username",
-            "bio",
-            "email",
-            "role",
+            'first_name', 'last_name', 'username',
+            'bio', 'email', 'role',
         )
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
 
     class Meta:
-        fields = ("id", "text", "author", "pub_date")
-        read_only_fields = ("title", "review", "author")
+        fields = ('id', 'text', 'author', 'pub_date')
+        read_only_fields = ('title', 'review', 'author')
         model = Comment
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
 
     def validate(self, data):
-        if self.context["request"].method != "POST":
+        if self.context['request'].method != 'POST':
             return data
-        user = self.context["request"].user
-        title_id = self.context["view"].kwargs.get("title_id")
+        user = self.context['request'].user
+        title_id = self.context['view'].kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
-        if Review.objects.filter(title=title, author=user).exists():
-            raise serializers.ValidationError(
-                "Пользователь может " "оставить только один отзыв."
-            )
+        if Review.objects.filter(title=title,
+                                 author=user).exists():
+            raise serializers.ValidationError('Пользователь может '
+                                              'оставить только один отзыв.')
         return data
 
     class Meta:
-        fields = ("id", "text", "author", "score", "pub_date")
-        read_only_fields = ("title", "author")
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        read_only_fields = ('title', 'author')
         model = Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
-        exclude = ("id",)
+        exclude = ('id',)
 
 
 class CategoryReadSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
-        exclude = ("id",)
+        exclude = ('id',)
 
 
 class GenreSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Genre
-        fields = ("name", "slug")
+        fields = ('name', 'slug')
 
 
 class TitleSerializer(serializers.ModelSerializer):
@@ -105,34 +111,43 @@ class TitleSerializer(serializers.ModelSerializer):
     rating = serializers.IntegerField(read_only=True)
 
     class Meta:
-        fields = ("id", "name", "year", "rating", "description", "genre", "category")
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
         model = Title
 
 
 class TitlePostSerializer(serializers.ModelSerializer):
     category = serializers.SlugRelatedField(
-        slug_field="slug", queryset=Category.objects.all()
+        slug_field='slug', queryset=Category.objects.all()
     )
     genre = serializers.SlugRelatedField(
-        slug_field="slug", queryset=Genre.objects.all(), many=True
+        slug_field='slug', queryset=Genre.objects.all(),
+        many=True
     )
 
     class Meta:
-        fields = ("id", "name", "year", "description", "genre", "category")
+        fields = (
+            'id', 'name', 'year', 'description', 'genre', 'category'
+        )
         model = Title
 
     def validate_year(self, value):
         year_today = datetime.date.today().year
         if value > year_today:
-            raise serializers.ValidationError("Проверьте год создания!")
+            raise serializers.ValidationError('Проверьте год создания!')
         return value
 
 
 class ReadOnlyTitleSerializer(serializers.ModelSerializer):
-    rating = serializers.IntegerField(source="reviews__score__avg", read_only=True)
+    rating = serializers.IntegerField(
+        source='reviews__score__avg', read_only=True
+    )
     genre = GenreSerializer(many=True)
     category = CategorySerializer()
 
     class Meta:
         model = Title
-        fields = ("id", "name", "year", "rating", "description", "genre", "category")
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
