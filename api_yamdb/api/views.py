379b861691d0a8c78b647eb1e799566651059ee0
Guzovvv <guzovvv@yandex.ru>
2022-12-21 17:28:36 +0300

    SP16v4

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -16,9 +16,9 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from reviews.models import Category, Genre, Review, Title, User
 
 from api_yamdb.settings import DEFAULT_FROM_EMAIL
-from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitlesFilter
 
